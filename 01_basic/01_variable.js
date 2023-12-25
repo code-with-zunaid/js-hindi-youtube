effe87aca2d473f35d4cae88ea487e3a23181875
Zunaid alam <148374715+code-with-zunaid@users.noreply.github.com>
2023-12-25 11:58:15 +0000

    converion of data type

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -1,7 +1,13 @@
 const id=134
 let email="zunaidalam7070@gmail.com"
 /* generally const and let is used 
-var has scope problem if another 
+var has block scope and functional scope problem  
+agar kisi file var name="zunaid" likha hai and 
+agar kio dusra programmer esi project ke kise diusre file me 
+var name="kadir" likh de to har jagah name=kadir ho jata yahi problem tha 
+var ke sath ek jagah chanege na ho kar pure project me chage ho jata tha 
+
+ */
 
 
 var account="sbi"
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,20 @@
+/*let score="98"
+console.log(typeof score);
+console.log(typeof(score));
+let cnvNumber=Number(score)
+console.log(cnvNumber);
+
+let score2="98avc"
+console.log(typeof score2);
+let score3=Number(score2)
+console.log(score3);// this print NaN its mean not a number
+let score4=true;
+
+console.log(typeof score4);
+let score5=Number(score4)
+console.log(score5);*/
+
+let loggediN=""
+console.log(loggediN);
+let boolean=Boolean(loggediN)
+console.log(boolean);
