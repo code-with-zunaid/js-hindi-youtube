563975943fc65a90cde31fcb5f241295ae92791d
Zunaid alam <148374715+code-with-zunaid@users.noreply.github.com>
2023-12-25 19:56:39 +0000

    final change

--- 01_basic/05_dataTypes-summery.js
+++ 01_basic/05_dataTypes-summery.js
@@ -31,5 +31,79 @@ console.log(id===id2);
 /*it return false because here symbol return a data type 
 and it is not necessary that it will return same data type for
 both id and id2;*/
+const BigIngum=9808776565443455678798n;
+//when we add n at the last of number then it become BigInt
+
+
+/*
+
+==============reference(non primitive)==========
+1.Array 
+it usese to [ ] square bracket;
+its return type is object
+
+const hero=["saktiman" , "dogo"]
+
+
+2.Object
+it uses { }
+and contain key value pair
+const obj1={
+        name:"ajay"
+        age:78
+}
+
+
+3.function 
+in javascript a function can be treated as variable
+its data type is function,(fuction object)
+return type is object function
+
+*/
+const myfunction=function()
+{
+console.log("Hello world");
+}
+
+
+const obj1={
+        name:"ajay",
+        age:78,
+}
+
+const hero=["saktiman" , "dogo"]
+console.log(typeof hero);
+
+
+
+/*
+=============Memory==========
+stack(primitive) and heap(non-primitive)
+
+let email="zunaidalam707070@gmail.com";
+let email2=email;
+console.log(email);
+console.log(email2);
+// here both print the same value
+email2="kad34@gmail.com";
+console.log(email);
+console.log(email2);
+/*here we change the value of email2 and it does not effect email value because
+in stack copy of email  is assigned to email2 so its actual value is not effected 
+but this is not happend in heap here change actual value
+*/ 
+let student=
+{
+        name1:"zunaid",
+        age:23,
+};
+
+let student2=student;
+student2.age=60;
+console.log(student.age);
+console.log(student2.age);
+//here both print 60 because here both contain refferance of same object (of heap memory)
+
+
 
 
