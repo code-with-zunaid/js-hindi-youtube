ece068cef2ced003a73664c514e51c689ab1b083
Zunaid alam <148374715+code-with-zunaid@users.noreply.github.com>
2023-12-25 11:22:23 +0000

    change in dataTypes

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,24 @@
+"use strict"//treat all js code as newer version
+
+
+//alert(3+4)
+/*we are using nodejs if we run this on
+ browser then this will run because in browser engine 
+ is exist to execute the java script */
+
+ console.log(3+3);
+
+ let name1="zunaid"
+ const a=234;
+ let city;
+ console.log(typeof name1);
+ console.log(typeof a);
+ console.log(typeof city)
+ console.log(typeof undefined);//udefined  ka data type undefined hi hai
+ console.log(typeof null);// null ka data type object hai;
+ //number 2 ka power 53
+// BigInt
+//String
+//null standalone value
+//Symbol=> unique
+//boolean=>true/false
